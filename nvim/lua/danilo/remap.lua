76bad78ee10491be116692af30fdf4b63b72caf3
danilopereirasilva <danilopereira.contato@gmail.com>
2024-06-16 21:53:53 -0300

    added nvim config files

---
+++ nvim/lua/danilo/remap.lua
@@ -0,0 +1,21 @@
+vim.g.mapleader = " "
+vim.keymap.set("n", "<leader>ge", vim.cmd.Ex)
+vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Moves selected line up (J) and down (K)
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Changes the content of paste
+vim.keymap.set("x", "<leader>p", "\"_dP")
+
+-- >>>>>>>>Rename every variable at the same time<<<<<<<<<
+vim.keymap.set("n", "<leader>s", ":%s/\\<<C-r><C-w>\\>/<C-r><C-w>/gI<Left><Left>")
+
+-- Diagnostic keymaps
+vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+vim.keymap.set("n", "<leader>u", vim.cmd.UndotreeToggle)
---
+++ nvim/lua/danilo/set.lua
@@ -0,0 +1,74 @@
+-- See highlight on search
+vim.opt.hlsearch = true
+
+-- -- Transparent background
+-- vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
+-- vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+-- Set cursor
+vim.opt.guicursor = ""
+
+-- Set number column
+vim.opt.number = true
+
+-- Set relative number navigation
+vim.opt.relativenumber = true
+
+-- Set tabstop definitions
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
+
+-- Unset line wrap
+vim.opt.wrap = false
+
+-- Disable vim backup but enable undotree long term backup
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+vim.opt.undofile  = true
+
+-- Nice colors
+vim.opt.termguicolors = true
+
+-- Set mouse integration
+vim.opt.mouse = 'a'
+--vim.opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  See `:help 'clipboard'`
+vim.opt.clipboard = 'unnamedplus'
+
+-- Save undo history
+vim.opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or more capital letters in the search term
+vim.opt.ignorecase = true
+vim.opt.smartcase = true
+
+-- Decrease update time
+vim.opt.updatetime = 250
+
+-- Delimitate width keepcoding
+vim.opt.colorcolumn = "80"
+
+-- Indentation
+vim.opt.smartindent = true
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+vim.opt.timeoutlen = 300
+
+-- Configure how new splits should be opened
+vim.opt.splitright = true
+vim.opt.splitbelow = true
+
+-- Preview substitutions live, as you type!
+vim.opt.inccommand = 'split'
+
+-- Show which line your cursor is on
+vim.opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+vim.opt.scrolloff = 10
